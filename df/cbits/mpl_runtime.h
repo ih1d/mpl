cb90758caf4b3d7dfc484cb0db22d9b1d2768593
Isaac Hiram Lopez Diaz <isaac.lopez@upr.edu>
2026-03-06 00:50:04 -0400

    removed, not faithful to capi

--- df/cbits/mpl_runtime.h
+++ df/cbits/mpl_runtime.h
@@ -8,17 +8,5 @@
 
 #include <arrow-glib/arrow-glib.h>
 
-/* DATA aliases */
-typedef GError                  Error;
-typedef GArrowInputStream       InputStream;
-typedef GArrowTable             Table;
-typedef GArrowCSVReaderClass    CsvReaderClass;
-typedef GArrowCSVReader         CsvReader;
-typedef GArrowCSVReadOptions    CsvReadOptions;
-
-/* Functions */
-CsvReadOptions* new_csv_read_options(void);
-CsvReader* new_csv_reader(InputStream* stream, CsvReadOptions* opts, Error** err);
-Table* read_csv_reader(CsvReader* reader, Error** err);
 
 #endif
